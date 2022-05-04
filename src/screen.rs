f57c5d519070b890bf1a033afbf00b09223bc536
gammelalf <31348226+gammelalf@users.noreply.github.com>
2022-05-04 10:54:34 +0200

    Added resizing anchors to docks

--- src/screen.rs
+++ src/screen.rs
@@ -1,8 +1,10 @@
+use std::cmp::max;
 use gloo::events::EventListener;
 use web_sys::HtmlElement;
 use yew::prelude::*;
 
 use crate::promise::PendingPromise;
+use crate::anchor::Anchor;
 use crate::window::{Window, WindowProps};
 
 #[derive(Properties, PartialEq)]
@@ -28,6 +30,11 @@ pub enum ScreenMsg {
     Resize,
     NewWindow(PendingPromise),
     DeleteWindow(u32),
+    ResizeDock(Dock, i32, i32),
+}
+#[derive(Copy, Clone)]
+pub enum Dock {
+    Top, Left, Bottom, Right
 }
 
 impl Component for Screen {
@@ -65,7 +72,7 @@ impl Component for Screen {
                 self.height = parent.offset_height() as u32;
                 true
             }
-            ScreenMsg::NewWindow(promise) => {
+            NewWindow(promise) => {
                 if self.next_window_id == u32::MAX {
                     promise.reject("Out of ids");
                     return false;
@@ -77,7 +84,7 @@ impl Component for Screen {
                 promise.resolve(id);
                 true
             },
-            ScreenMsg::DeleteWindow(id) => {
+            DeleteWindow(id) => {
                 let mut index = None;
                 for (i, props) in self.windows.iter().enumerate() {
                     if props.id == (id as u32).into() {
@@ -93,25 +100,44 @@ impl Component for Screen {
                     false
                 }
             }
+            ResizeDock(dock, dx, dy) => {
+                use Dock::*;
+                match dock {
+                    Top    => { self.top_dock    = max(0, self.top_dock    + dy); },
+                    Left   => { self.left_dock   = max(0, self.left_dock   + dx); },
+                    Bottom => { self.bottom_dock = max(0, self.bottom_dock - dy); },
+                    Right  => { self.right_dock  = max(0, self.right_dock  - dx); },
+                }
+                true
+            }
         }
     }
 
-    fn view(&self, _ctx: &Context<Self>) -> Html {
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        let on_move = |dock| ctx.link().callback(move |(dx, dy)|
+            ScreenMsg::ResizeDock(dock, dx, dy)
+        );
         let windows = self.windows.iter().map(|props| html!{<Window ..props.clone()/>});
         return html!{
             <>
-                <div class="waw-docks" style={
-                    format!("--left: {}px; --top: {}px; --right: {}px; --bottom: {}px;",
-                        self.left_dock, self.top_dock, self.right_dock, self.bottom_dock
-                    )
-                }>
+                <div class="waw-docks" style={ format!("--left: {}px; --top: {}px; --right: {}px; --bottom: {}px;", self.left_dock, self.top_dock, self.right_dock, self.bottom_dock) }>
                     <div class="waw-taskbar"/>
-                    <div class="waw-top-dock"/>
-                    <div class="waw-left-dock"/>
-                    <div class="waw-bottom-dock"/>
-                    <div class="waw-right-dock"/>
-                    <div class="waw-center-dock">
-                        {format!("Your screen is {} by {} pixel", self.width, self.height)}
+                    <div class="waw-center-dock"/>
+                    <div class="waw-left-dock">
+                        <Anchor class="waw-e" on_move={on_move(Dock::Left)}/>
+                        <div class="waw-flex-vertical"/>
+                    </div>
+                    <div class="waw-right-dock">
+                        <Anchor class="waw-w" on_move={on_move(Dock::Right)}/>
+                        <div class="waw-flex-vertical"/>
+                    </div>
+                    <div class="waw-top-dock">
+                        <Anchor class="waw-s" on_move={on_move(Dock::Top)}/>
+                        <div class="waw-flex-horizontal"/>
+                    </div>
+                    <div class="waw-bottom-dock">
+                        <Anchor class="waw-n" on_move={on_move(Dock::Bottom)}/>
+                        <div class="waw-flex-horizontal"/>
                     </div>
                 </div>
                 {for windows}
